c4da380c038f8030c214d72c341940e97522d319
Christopher McHenry <cmaq@Christophers-MacBook-Air-3.local>
2023-10-16 13:08:12 -0400

    error 403

--- msgkit/errorlog.py
+++ msgkit/errorlog.py
@@ -36,6 +36,28 @@ class ErrorLog:
                 'Check if cache and content management systems are interfering',
             ]
             self.printSolutions(solutions)
+        elif str(num) == '403':
+            causes = [
+                'The client lacks the necessary permissions to access this source (lack of authentification, not having appropriate credentials, an access control list that prohibits access, etc)',
+                'If the credentials provided for authentification or incorrect or nonexistent'
+                'Your IP may be prohibited from accessing the source (blacklisted) or has not been granted access (whitelisted)'
+                'You may have directory listing disabled, meaning you will not be able to view contents of the directory',
+                'You may not possess permissions to access a file on the requested source',
+                'A firewall, Web Application Firewall (WAF) or another web application security device may have blocked your request to access the source',
+                'The client may have exceeded the amount of requests alotted to it by the website or API',
+                'Source may have restrictions on what methods of HTML can be used',
+                'The website may be using a specific Referer in its header that may be missing or incorrect in the request, triggering a 403 error',
+                'There may be a misconfiguration of the web server or its security settings, which could lead to a 403 error',
+            ]
+            self.printCauses(causes)
+            print('\nPossible solutions:')
+            solutions = [
+                'Check permissions for the website and/or its resources',
+                'Be sure to ensure proper authentification',
+                'Address the application\'s or server\'s security measures (e.g. Disable VPN/Hotspot, make fewer requests to access the source, etc.)',
+                'Enable directory listing',
+            ]
+            self.printSolutions(solutions)
 
     def printCauses(self, causes):
         if self.causenum != 0:
